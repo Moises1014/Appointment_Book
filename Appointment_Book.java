1cc47376dbbaf63681df1524d4aa2e52953431d2
Moises1014 <48698541+Moises1014@users.noreply.github.com>
2019-05-22 15:51:44 -0400

    Add files via upload

---
+++ Appointment_Book.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appointment_book;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author moises
+ */
+public class Appointment_Book {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            // TODO code application logic here
+            Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/appointmentbook");
+            view view = new view();
+            view.setVisible(true);
+        } catch (SQLException ex) {
+            Logger.getLogger(Appointment_Book.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        view view = new view();
+        view.setVisible(true);
+    }
+
+}
